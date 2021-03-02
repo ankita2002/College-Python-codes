b8120a9f994b1eaf13cb423544c3df3e8b483373
Ankita <ankitaupadhyaya209@gmail.com>
2021-03-02 10:53:00 +0530

    Create Minimum & Maximum of decimal array.py
     Output:
    ******************Minimum & Maximum of Decimal Array********************
    Maximum Value :  89.56
    Minimum Value :  5.09

---
+++ Minimum & Maximum of decimal array.py
@@ -0,0 +1,5 @@
+print("******************Minimum & Maximum of Decimal Array********************")
+import numpy as np
+arrList = np.array([48.5,67.9,78.65,89.56,78.09,5.09])
+print("Maximum Value : ",arrList.max())
+print("Minimum Value : ",arrList.min())
